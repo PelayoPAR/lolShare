4b1cd36ba6f19842fe0081716c5ba031d0badb56
PelayoPAR <pelayopar@gmail.com>
2022-08-27 14:20:11 +0200

    added user update link

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -7,8 +7,20 @@ router.get("/:userId", isLoggedIn, async (req, res) => {
   const userId = req.params.userId;
   try {
     const userInfo = await User.findById(userId);
-    console.log(userInfo);
     return res.render("user/user");
+  } catch (err) {
+    console.error(err);
+    return res.status(500).render("/", { errorMessage: err.message });
+  }
+});
+
+router.get("/:userId/update-user", isLoggedIn, async (req, res) => {
+  const userId = req.params.userId;
+  console.log("USER ID:", req.params.userId);
+  try {
+    const userInfo = await User.findById(userId);
+
+    return res.render("user/update-user");
   } catch (err) {
     console.error(err);
     return res.status(500).render("/", { errorMessage: err.message });
