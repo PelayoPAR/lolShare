a5be3cbccc11892deeddad4ace5b717763edb337
PelayoPAR <pelayopar@gmail.com>
2022-08-27 20:50:59 +0200

    added Meme model

---
+++ models/Meme.model.js
@@ -0,0 +1,24 @@
+const { Schema, model } = require("mongoose");
+
+const MemeSchema = new Schema({
+  id: {
+    type: String,
+    unique: true,
+  },
+  title: {
+    type: String,
+  },
+  postLink: {
+    type: String,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+  thumbnail: {
+    type: String,
+  },
+  subreddit: {
+    type: String,
+  },
+});
