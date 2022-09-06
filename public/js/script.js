06df87220e3908a1c78ac93fbe6d47e2b2f5ad18
PelayoPAR <pelayopar@gmail.com>
2022-09-06 16:41:57 +0200

    trying to implement user changes to the fetch -in progress -

--- public/js/script.js
+++ public/js/script.js
@@ -1,7 +1,19 @@
+let fetchButton;
+
 document.addEventListener(
   "DOMContentLoaded",
   () => {
     console.log("lolShare JS imported successfully!");
+    fetchButton = document.querySelector("#fetchButton");
+    fetchButton.onClick;
   },
   false
 );
+
+async function fetchMemes() {
+  const memesResponse = await fetch(
+    "https://api.reddit.com/r/dankmemes/top/?limit=10"
+  );
+  const memes = await memesResponse.json();
+  console.log(memes);
+}
