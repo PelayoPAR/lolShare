34e15f6d1530068cc141dee9b181018c8b38f44e
Asem Edress <asem.edress@gmail.com>
2022-09-06 20:11:18 +0200

    minor css changes after a conflict

--- public/js/script.js
+++ public/js/script.js
@@ -1,19 +1,19 @@
-let fetchButton;
+//let fetchButton;
 
 document.addEventListener(
   "DOMContentLoaded",
   () => {
     console.log("lolShare JS imported successfully!");
-    fetchButton = document.querySelector("#fetchButton");
-    fetchButton.onClick;
+    //  fetchButton = document.querySelector("#fetchButton");
+    //  fetchButton.onClick;
   },
   false
 );
 
-async function fetchMemes() {
+/*async function fetchMemes() {
   const memesResponse = await fetch(
     "https://api.reddit.com/r/dankmemes/top/?limit=10"
   );
   const memes = await memesResponse.json();
   console.log(memes);
-}
+}*/
